a9517888cb1bbdf55d2db6f450fb891c28d374dc
Christopher Ing <ing.chris@gmail.com>
2011-12-19 13:43:02 -0500

    Added Sequence class in database

--- app_server_db.py
+++ app_server_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, DateTime, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
-import datetime
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -21,7 +21,7 @@ class Job(Base):
     #this is a one-to-many relationship one job to replicas
     #this should connect with the following class in the Replica table
     #job_id = Column(Integer, ForeignKey('job.id'))
-    replicas = relationship("Replica", backref="job")
+    replicas = relationship("Replica")
 
     pbs_id = Column(Integer)
     pbs_num_of_nodes = Column(Integer)
@@ -45,6 +45,24 @@ class Replica(Base):
     status = Column(String)
     reaction_coordinate = Column(Float)
 
+    sequences = relationship("Sequence")
+
     def __init__(self, reaction_coordinate):
         self.reaction_coordinate = reaction_coordinate
         self.start_time = datetime.now()
+
+
+class Sequence(Base):
+    '''
+    This represents a row in our Seqeuence database
+    '''
+
+    __tablename__ = 'sequence'
+
+    id = Column(Integer, primary_key=True)
+    replica_id = Column(Integer, ForeignKey('replica.id'))
+
+    sequence_number = Column(Integer)
+
+    def __init__(self, sequence_number):
+        self.sequence_number = sequence_number
